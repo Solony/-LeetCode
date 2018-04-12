95265bb2b0941bb9797cde18eb5f96a6b11a74b8
Solony <hasd093156@gmail.com>
2018-04-12 09:50:59 -0500

    Create Two Sum.py

---
+++ Two Sum.py
@@ -0,0 +1,50 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ans = []
+        i = -1
+        while(i < len(nums)):
+            i += 1
+            for j in xrange(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    ans.append(i)
+                    ans.append(j)
+                    return ans
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+def stringToInt(input):
+    return int(input)
+
+def integerListToString(nums, len_of_list=None):
+    if not len_of_list:
+        len_of_list = len(nums)
+    return json.dumps(nums[:len_of_list])
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+    lines = readlines()
+    while True:
+        try:
+            line = lines.next()
+            nums = stringToIntegerList(line)
+            line = lines.next()
+            target = stringToInt(line)
+            
+            ret = Solution().twoSum(nums, target)
+
+            out = integerListToString(ret)
+            print out
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
