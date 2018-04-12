2be60e4c8c4a7a58337ce6ee15335ada8c66f494
Solony <hasd093156@gmail.com>
2018-04-12 09:59:38 -0500

    Create Median of Two Sorted Arrays.py

---
+++ Median of Two Sorted Arrays.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        from statistics import median
+        return median(sorted(nums1 + nums2))
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            nums1 = stringToIntegerList(line);
+            line = next(lines)
+            nums2 = stringToIntegerList(line);
+            
+            ret = Solution().findMedianSortedArrays(nums1, nums2)
+
+            out = str(ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
