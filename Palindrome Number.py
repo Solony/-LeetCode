a5c3544167cea293f003d9aa0e9f119b1efcad3a
Solony <hasd093156@gmail.com>
2018-04-12 09:54:19 -0500

    Create Palindrome Number.py

---
+++ Palindrome Number.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        else: return int(str(x)[::-1]) == x
+
+def stringToInt(input):
+    return int(input)
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+    lines = readlines()
+    while True:
+        try:
+            line = lines.next()
+            x = stringToInt(line)
+            
+            ret = Solution().isPalindrome(x)
+
+            out = (ret)
+            print out
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
